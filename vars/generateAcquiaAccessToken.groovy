2ebc76d16c71fba2251faf7a0ad942dc0c2e1d7b
Aman Srivastava <aman.srivastava@acquia.com>
2025-02-09 18:11:20 +0530

    Add generate token script.

--- vars/generateAcquiaAccessToken.groovy
+++ vars/generateAcquiaAccessToken.groovy
@@ -1,21 +1,26 @@
 // vars/generateAccessToken.groovy
+
 def call() {
     import groovy.json.JsonSlurper
 
-    def clientId = credentials('oauth-client-id') // Use Jenkins credentials binding
-    def clientSecret = credentials('oauth-client-secret')
-    def connection = new URL("https://accounts.acquia.com/api/auth/oauth/token").openConnection()
-    connection.setRequestMethod('POST')
-    connection.setDoOutput(true)
-    connection.setRequestProperty('Content-Type', 'application/x-www-form-urlencoded')
+    // Use Jenkins credentials binding to access the stored credentials
+    withCredentials([usernamePassword(credentialsId: 'acquia-cloud-auth', 
+                                      usernameVariable: 'CLIENT_ID', 
+                                      passwordVariable: 'CLIENT_SECRET')]) {
+        def tokenUrl = 'https://accounts.acquia.com/api/auth/oauth/token'
+        def connection = new URL(tokenUrl).openConnection()
+        connection.setRequestMethod('POST')
+        connection.setDoOutput(true)
+        connection.setRequestProperty('Content-Type', 'application/x-www-form-urlencoded')
 
-    def postData = "grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}"
-    connection.outputStream.withWriter { writer ->
-        writer << postData
+        def postData = "grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}"
+        connection.outputStream.withWriter { writer ->
+            writer << postData
+        }
+
+        def response = connection.inputStream.text
+        def jsonResponse = new JsonSlurper().parseText(response)
+
+        return jsonResponse.access_token
     }
-
-    def response = connection.inputStream.text
-    def jsonResponse = new JsonSlurper().parseText(response)
-
-    return jsonResponse.access_token
 }
