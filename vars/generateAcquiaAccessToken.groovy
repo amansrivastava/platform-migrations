fad6513fe1798ec80bdaa69dd3ef059ef5240d93
Aman Srivastava <aman.srivastava@acquia.com>
2025-02-09 19:43:42 +0530

    Updated accessToken.

--- vars/generateAcquiaAccessToken.groovy
+++ vars/generateAcquiaAccessToken.groovy
@@ -14,10 +14,7 @@ def call() {
         connection.setRequestProperty('Content-Type', 'application/x-www-form-urlencoded')
 
         def postData = "grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}"
-        connection.outputStream.withWriter { writer ->
-            writer << postData
-        }
-
+        connection.outputStream.write(postData.getBytes("UTF-8"))
         def response = connection.inputStream.text
         def jsonResponse = new JsonSlurper().parseText(response)
 
